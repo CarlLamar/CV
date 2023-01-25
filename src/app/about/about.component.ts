5a402acfabc5c35d8998636fb7cf4d1e2ec297c2
CarlLamar <100596478+CarlLamar@users.noreply.github.com>
2023-01-25 19:40:11 +0800

    2 files modified

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-//Property Binding
+//Interpolation
 export class AboutComponent {
   appName = ' Carl D. Gegante ';
   appEmail = 'cdgegante@gmail.com';
